39074b2972074a0601270c55fbedb51844894cd6
Adryel <adryelcesarm@gmail.com>
2024-10-22 11:34:56 -0300

    cashwave on

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,7 +15,7 @@ export function HeaderContainer() {
 				<h3>Cash<span className='WaveName'>Wave</span></h3>
 			</Link>
 			<nav className="NavHeader">
-				{/* <a href="https://app.gateway.cashtimepay.com.br/register" className='LinkAcessHeader' target='_blank' >Acessar Gateway</a> */}
+				
 				{/* <a href='https://app.cashtimepayments.com.br/register' target='_blank'><button className='SignUpButtonHeader'>Acessar Checkout</button></a> */}
 			</nav>
 		</div>
